0d5be2e309a31eb166b4fd057d7d149854cdb71f
Khabat Setaei <30699680+XS227@users.noreply.github.com>
2026-09-25 13:38:36 +0330

    Update global cache schema test for TMX v17

--- analysis/tests/test_global_universe_cache_schema.py
+++ analysis/tests/test_global_universe_cache_schema.py
@@ -1,5 +1,5 @@
 from global_markets.cached_universe import CACHE_SCHEMA_VERSION
 
 
-def test_global_universe_cache_schema_is_jpx_official_v16():
-    assert CACHE_SCHEMA_VERSION == 16
+def test_global_universe_cache_schema_is_tmx_official_v17():
+    assert CACHE_SCHEMA_VERSION == 17
