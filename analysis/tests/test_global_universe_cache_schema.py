cd492ab338f2430c1e72c85fdd859c0084e41683
Khabat Setaei <30699680+XS227@users.noreply.github.com>
2026-09-25 13:09:35 +0330

    Update global universe schema expectation for LSE v15

--- analysis/tests/test_global_universe_cache_schema.py
+++ analysis/tests/test_global_universe_cache_schema.py
@@ -1,5 +1,5 @@
 from global_markets.cached_universe import CACHE_SCHEMA_VERSION
 
 
-def test_global_universe_cache_schema_is_bist_official_v14():
-    assert CACHE_SCHEMA_VERSION == 14
+def test_global_universe_cache_schema_is_lse_official_v15():
+    assert CACHE_SCHEMA_VERSION == 15
